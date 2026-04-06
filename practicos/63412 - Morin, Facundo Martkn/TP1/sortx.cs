fade9cb126f015536b557703ca96c4f8b0f82894
Facundo Morán <facumamoran@gmail.com>
2026-04-06 20:41:03 -0300

    Modelo de configuración establecido

--- practicos/63412 - Morin, Facundo Martkn/TP1/sortx.cs
+++ practicos/63412 - Morin, Facundo Martkn/TP1/sortx.cs
@@ -4,4 +4,14 @@
 //       [-d|--delimiter delimitador]
 //       [-nh|--no-header] [-h|--help]
 
-Console.WriteLine($"sortx {string.Join(" ", args)}");
+// Console.WriteLine($"sortx {string.Join(" ", args)}");
+
+record SortField(string Name, bool Numeric, bool Descending);
+
+record AppConfig(
+    string?         InputFile,
+    string?         OutputFile,
+    string          Delimiter,
+    bool            NoHeader,
+    List<SortField> SortFields
+);
