c24ae41d7e5783aef7d856959067232d2bb493ab
EmmanuelGR-web <emmanuelgonzalez08.eg@gmail.com>
2026-04-06 14:54:59 -0300

    Agrego instrucciones para usar app

--- practicos/63182 - Gonzalez Rojas, Emmanuel/TP1/sortx.cs
+++ practicos/63182 - Gonzalez Rojas, Emmanuel/TP1/sortx.cs
@@ -1,7 +1,22 @@
+using static system.Console;
+using System;
+using System.IO;
+using System.Collections.Generic;
 
-// sortx [input [output]] [-b|--by campo[:tipo[:orden]]]...
-//       [-i|--input input] [-o|--output output]
-//       [-d|--delimiter delimitador]
-//       [-nh|--no-header] [-h|--help]
+WriteLine(@"
+Indicaciones para usar sortx:
 
-Console.WriteLine($"sortx {string.Join(" ", args)}");
+sortx [archivoEntrada [archivoSalida]] -b campo[:tipo[:orden]]
+
+Ejemplo:
+sortx empleados.csv -b apellido
+sortx empleados.csv -b edad:num:desc
+
+Opciones:
+  -b, --by            Campo de ordenamiento
+  -i, --input         Archivo de entrada
+  -o, --output        Archivo de salida
+  -d, --delimiter     Delimitador (ej: , | \t)
+  -nh, --no-header    Indica que no hay encabezado
+  -h, --help          Mostrar ayuda
+");
