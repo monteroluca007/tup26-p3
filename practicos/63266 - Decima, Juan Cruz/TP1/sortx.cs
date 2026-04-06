c16e99b017e2654d45d990f3e2e3b183a38a89a7
Juan Cruz Decima <devjuancruz003@gmail.com>
2026-04-06 20:50:11 -0300

    Definición de record establecida.
    Se estableció el record sugerido en el tp.

--- practicos/63266 - Decima, Juan Cruz/TP1/sortx.cs
+++ practicos/63266 - Decima, Juan Cruz/TP1/sortx.cs
@@ -5,3 +5,14 @@
 //       [-nh|--no-header] [-h|--help]
 
 Console.WriteLine($"sortx {string.Join(" ", args)}");
+
+
+record SortField(string Name, bool Numeric, bool Descending);
+
+record AppConfig(
+    string?         InputFile,
+    string?         OutputFile,
+    string          Delimiter,
+    bool            NoHeader,
+    List<SortField> SortFields
+);
