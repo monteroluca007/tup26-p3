4bb15415e6927824f86724b91970ab73a4ac6af9
Tomas Gomez <tomasgomezzi36gmail.com>
2026-04-08 14:38:35 -0300

    feat: estructura principal del pipeline y modelos de datos

--- practicos/61489 - Gomez, Tomis/TP1/sortx.cs
+++ practicos/61489 - Gomez, Tomis/TP1/sortx.cs
@@ -1,3 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+try
+{
+    var config = LeerArgumentos(args);
+    var contenido = LeerEntrada(config);
+    var filas = ProcesarTexto(contenido, config);
+    var filasOrdenadas = OrdenarFilas(filas, config);
+    var resultado = Serializar(filasOrdenadas, config);
+    EscribirSalida(resultado, config);
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Error: {ex.Message}");
+    Environment.Exit(1);
+}
 
 // sortx [input [output]] [-b|--by campo[:tipo[:orden]]]...
 //       [-i|--input input] [-o|--output output]
