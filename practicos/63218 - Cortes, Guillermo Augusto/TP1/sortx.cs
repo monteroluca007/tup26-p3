b70d74baaf0cd6d7b796a7b4e549538a2301a9fa
GuilleCortes01 <guillecortes100@gmail.com>
2026-04-07 22:45:52 -0300

    agregue la funcionalidad de ordenamiento en SortRows para procesar campos de ordenación

--- practicos/63218 - Cortes, Guillermo Augusto/TP1/sortx.cs
+++ practicos/63218 - Cortes, Guillermo Augusto/TP1/sortx.cs
@@ -187,9 +187,43 @@ List<Dictionary<string, string>> ParseDelimited(string text, AppConfig config)
 
 List<Dictionary<string, string>> SortRows(List<Dictionary<string, string>> rows, AppConfig config)
 {
-    return rows;
-}
+    if (rows.Count == 0)
+        return rows;
 
+    IOrderedEnumerable<Dictionary<string, string>>? ordered = null;
+
+    foreach (var field in config.SortFields)
+    {
+        Func<Dictionary<string, string>, object> key = row =>
+        {
+            if (!row.ContainsKey(field.Name))
+                throw new Exception($"Columna inexistente: {field.Name}");
+
+            var value = row[field.Name];
+
+            if (field.Numeric)
+            {
+                if (!double.TryParse(value, out var num))
+                    throw new Exception($"Valor no numérico: {value}");
+
+                return num;
+            }
+
+            return value;
+        };
+
+        if (ordered == null)
+        {
+            ordered = field.Descending ? rows.OrderByDescending(key) : rows.OrderBy(key);
+        }
+        else
+        {
+            ordered = field.Descending ? ordered.ThenByDescending(key) : ordered.ThenBy(key);
+        }
+    }
+
+    return ordered.ToList();
+}
 string Serialize(List<Dictionary<string, string>> rows, AppConfig config)
 {
     return "";
