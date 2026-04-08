94d310d60b5a62c559b8875d07c443f098d8cac0
ValentinaPereyra03 <valentinanazarena19@gmail.com>
2026-04-08 19:43:07 -0300

    Update sortx.cs

--- practicos/63420 - Pereyra, Valentina Nazare/TP1/sortx.cs
+++ practicos/63420 - Pereyra, Valentina Nazare/TP1/sortx.cs
@@ -5,124 +5,67 @@
 //       [-nh|--no-header] [-h|--help]
 
 Console.WriteLine($"sortx {string.Join(" ", args)}");
+
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
-record CampoOrden(string Nombre, bool Descendente);
-
-record Configuracion(string Entrada, string Salida, bool Descendente, List<CampoOrden> Campos);
-
-class Program
+class SortX
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Iniciando aplicación...");
+        Console.WriteLine("=== PROGRAMA DE ORDENAMIENTO ===");
+
+        int cantidad = PedirCantidad();
+        int[] numeros = CargarNumeros(cantidad);
+
+        OrdenarNumeros(numeros);
+
+        MostrarNumeros(numeros);
     }
 
-    static Configuracion ObtenerConfiguracion(string[] args)
+    static int PedirCantidad()
     {
-        throw new NotImplementedException();
+        Console.Write("Ingrese la cantidad de números a ordenar: ");
+        return int.Parse(Console.ReadLine());
     }
 
-    static List<string> LeerArchivo(string ruta)
+    static int[] CargarNumeros(int cantidad)
     {
-        throw new NotImplementedException();
-    }
+        int[] arreglo = new int[cantidad];
 
-    static List<string> OrdenarLineas(List<string> lineas, Configuracion config)
-    {
-        throw new NotImplementedException();
-    }
-
-    static void GuardarArchivo(string ruta, List<string> lineas)
-    {
-        throw new NotImplementedException();
-    }
-
-    static void MostrarAyuda()
-    {
-        throw new NotImplementedException();
-    }
-}
-
-static Configuracion ObtenerConfiguracion(string[] args)
-{
-    string? entrada = null;
-    string? salida = null;
-    bool descendente = false;
-
-    foreach (var arg in args)
-    {
-        if (arg.StartsWith("--entrada="))
-            entrada = arg.Replace("--entrada=", "");
-
-        else if (arg.StartsWith("--salida="))
-            salida = arg.Replace("--salida=", "");
-
-        else if (arg == "--desc")
-            descendente = true;
-
-        else if (arg == "--help" || arg == "-h")
+        for (int i = 0; i < cantidad; i++)
         {
-            MostrarAyuda();
-            Environment.Exit(0);
+            Console.Write("Ingrese el número " + (i + 1) + ": ");
+            arreglo[i] = int.Parse(Console.ReadLine());
+        }
+
+        return arreglo;
+    }
+
+    static void OrdenarNumeros(int[] arreglo)
+    {
+        for (int i = 0; i < arreglo.Length - 1; i++)
+        {
+            for (int j = 0; j < arreglo.Length - i - 1; j++)
+            {
+                if (arreglo[j] > arreglo[j + 1])
+                {
+                    int temp = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = temp;
+                }
+            }
         }
     }
 
-    if (entrada == null || salida == null)
-        throw new ArgumentException("Debe indicar --entrada y --salida");
-
-    return new Configuracion(entrada, salida, descendente, new List<CampoOrden>());
-}
-
-static List<string> LeerArchivo(string ruta)
-{
-    if (!File.Exists(ruta))
-        throw new FileNotFoundException("El archivo indicado no existe.");
-
-    return File.ReadAllLines(ruta).ToList();
-}
-
-static void GuardarArchivo(string ruta, List<string> lineas)
-{
-    File.WriteAllLines(ruta, lineas);
-}
-
-static List<string> OrdenarLineas(List<string> lineas, Configuracion config)
-{
-    if (config.Descendente)
-        return lineas.OrderByDescending(x => x).ToList();
-
-    return lineas.OrderBy(x => x).ToList();
-}
-
-static void Main(string[] args)
-{
-    try
+    static void MostrarNumeros(int[] arreglo)
     {
-        var config = ObtenerConfiguracion(args);
-        var lineas = LeerArchivo(config.Entrada);
-        var ordenadas = OrdenarLineas(lineas, config);
-        GuardarArchivo(config.Salida, ordenadas);
+        Console.WriteLine("\nNúmeros ordenados:");
 
-        Console.WriteLine("Archivo procesado correctamente.");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {ex.Message}");
-    }
-}
+        foreach (int numero in arreglo)
+        {
+            Console.Write(numero + " ");
+        }
 
-static void MostrarAyuda()
-{
-    Console.WriteLine("Uso del programa:");
-    Console.WriteLine("  ordenador --entrada=archivo.txt --salida=resultado.txt [--desc]");
-    Console.WriteLine();
-    Console.WriteLine("Opciones:");
-    Console.WriteLine("  --entrada=RUTA     Archivo de entrada");
-    Console.WriteLine("  --salida=RUTA      Archivo de salida");
-    Console.WriteLine("  --desc             Orden descendente");
-    Console.WriteLine("  -h, --help         Mostrar ayuda");
+        Console.WriteLine();
+    }
 }
