1a931a83e8750503bee246e1996496d78f667f73
Vale del Rien <valedelrien@gmail.com>
2026-04-08 17:03:03 -0300

    Agregue la lógica de ParseArgs para interpretar argumentos
    En este commit implementé la función ParseArgs, que me permite interpretar los argumentos ingresados por la consola. Manejo de opciones como archivo de entrada, salida, delimitador y criterios de ordenamiento.

--- practicos/63220 - Pérez del Riem, Valentina/TP1/sortx.cs
+++ practicos/63220 - Pérez del Riem, Valentina/TP1/sortx.cs
@@ -24,3 +24,81 @@ catch (Exception ex)
     Console.Error.WriteLine("error: "+ ex.Message);
     Environment.Exit(1);
 }
+
+// 1. ParseArgs
+
+AppConfig ParseArgs(string[] argumentos)
+{
+    string? input = null;
+    string? output = null;
+    string separador = ",";
+    bool sinHeader = false;
+    List<SortField> reglas = new List<SortField>();
+    List<string> posicionales = new List<string>();
+
+      for (int i = 0; i < argumentos.Length; i++)
+    {
+        string actual = argumentos[i].ToLower();
+
+        switch (actual)
+        {
+            case "-h":
+            case "--help":
+                MostrarAyuda();
+                Environment.Exit(0);
+                break;
+
+            case "-i":
+            case "--input":
+                if (i + 1 >= argumentos.Length)
+                    throw new Exception("Falta el archivo de entrada.");
+
+                input = argumentos[++i];
+                break;
+
+            case "-o":
+            case "--output":
+                if (i + 1 >= argumentos.Length)
+                    throw new Exception("Falta el archivo de salida.");
+
+                output = argumentos[++i];
+                break;
+
+            case "-d":
+            case "--delimiter":
+                if (i + 1 >= argumentos.Length)
+                    throw new Exception("Falta el delimitador.");
+
+                separador = argumentos[++i];
+
+                if (separador == "\\t")
+                    separador = "\t";
+
+                break;
+
+            case "-nh":
+            case "--no-header":
+                sinHeader = true;
+                break;
+
+            case "-b":
+            case "--by":
+                if (i + 1 >= argumentos.Length)
+                    throw new Exception("Falta el valor para -b / --by.");
+
+                reglas.Add(ParseSortField(argumentos[++i]));
+                break;
+
+            default:
+                if (!argumentos[i].StartsWith("-"))
+                {
+                    posicionales.Add(argumentos[i]);
+                }
+                else
+                {
+                    throw new Exception("Opción no válida: " + argumentos[i]);
+                }
+                break;
+        }
+    } 
+}
