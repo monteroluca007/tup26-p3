311a3a29eae23498fb2848d9727997d2dcc8985f
sofisanchez126 <sofisanchez3333@gmail.com>
2026-04-08 18:10:54 -0300

    Agregar ordenamiento y serialización

--- practicos/63216 - Sanchez, Sofía Micaela/TP1/sortx.cs
+++ practicos/63216 - Sanchez, Sofía Micaela/TP1/sortx.cs
@@ -9,7 +9,9 @@ try
     var config = ParseArgs(args);
     var texto = ReadInput(config);
     var filas = ParseDelimited(texto, config);
-    Console.WriteLine($"Filas leídas: {filas.Count}");
+    var ordenadas = SortRows(filas, config);
+    var salida = Serialize(ordenadas, config);
+    WriteOutput(salida, config);
 }
 catch (Exception ex)
 {
@@ -24,15 +26,27 @@ AppConfig ParseArgs(string[] args)
 
 string ReadInput(AppConfig config)
 {
-    if (Console.IsInputRedirected)
-        return Console.In.ReadToEnd();
-
     return "";
 }
 
 List<Dictionary<string, string>> ParseDelimited(string texto, AppConfig config)
 {
     return new List<Dictionary<string, string>>();
+}
+
+List<Dictionary<string, string>> SortRows(List<Dictionary<string, string>> filas, AppConfig config)
+{
+    return filas;
+}
+
+string Serialize(List<Dictionary<string, string>> filas, AppConfig config)
+{
+    return "";
+}
+
+void WriteOutput(string salida, AppConfig config)
+{
+    Console.WriteLine(salida);
 }
 
 record SortField(string Name, bool Numeric, bool Descending);
