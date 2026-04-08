e20c946a68c2036291744354a715dc30b6a94e91
Vale del Rien <valedelrien@gmail.com>
2026-04-08 16:19:43 -0300

    Primer avance del programa sortx
    En este commit empecé a armar el programa, ya dejé lista la estructura principal para que después sea más fácil ir completando cada función paso por paso.

--- practicos/63220 - Pérez del Riem, Valentina/TP1/sortx.cs
+++ practicos/63220 - Pérez del Riem, Valentina/TP1/sortx.cs
@@ -1,7 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
 
 // sortx [input [output]] [-b|--by campo[:tipo[:orden]]]...
 //       [-i|--input input] [-o|--output output]
 //       [-d|--delimiter delimitador]
 //       [-nh|--no-header] [-h|--help]
-
-Console.WriteLine($"sortx {string.Join(" ", args)}");
+try
+{
+    Console.WriteLine($"sortx {string.Join(" ", args)}");
+    
+    AppConfig config = ParseArgs(args);
+    string texto = ReadInput(config);
+    var datos = ParseDelimited(texto,config);
+    List<Dictionary<string, string>> filasOrdenadas = SortRows(datos.Rows, datos.Headers, config);
+    string salida = Serialize(filasOrdenadas,datos.Headers,config);
+    WriteOutput(salida,config);
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine("error: "+ ex.Message);
+    Environment.Exit(1);
+}
