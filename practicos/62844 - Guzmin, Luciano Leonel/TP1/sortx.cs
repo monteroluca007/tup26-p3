1464701f5417f31ab054a4becc7e53c33f32648f
LucianoLeonelGuzman <guzmanlucianoleonel@gmail.com>
2026-04-08 21:24:22 -0300

    Creo una funcion para parsear argumentos permitiendo opciones de entrada, salida, delimitador, encabezado y campos de ordenamiento

--- practicos/62844 - Guzmin, Luciano Leonel/TP1/sortx.cs
+++ practicos/62844 - Guzmin, Luciano Leonel/TP1/sortx.cs
@@ -1,3 +1,65 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+AppConfig ParsearArgumentos(string[] args)
+{
+    string? archivoEntrada = null;
+    string? archivoSalida = null;
+    string delimitador = ",";
+    bool sinEncabezado = false;
+
+    var camposOrden = new List<SortField>();
+
+    for (int i = 0; i < args.Length; i++)
+    {
+        switch (args[i])
+        {
+            case "-i":
+            case "--input":
+                archivoEntrada = args[++i];
+                break;
+
+            case "-o":
+            case "--output":
+                archivoSalida = args[++i];
+                break;
+
+            case "-d":
+            case "--delimiter":
+                var d = args[++i];
+                delimitador = d == "\\t" ? "\t" : d;
+                break;
+
+            case "-nh":
+            case "--no-header":
+                sinEncabezado = true;
+                break;
+
+            case "-b":
+            case "--by":
+                camposOrden.Add(ParsearCampoOrden(args[++i]));
+                break;
+
+            case "-h":
+            case "--help":
+                MostrarAyuda();
+                Environment.Exit(0);
+                break;
+
+            default:
+                if (archivoEntrada == null) archivoEntrada = args[i];
+                else if (archivoSalida == null) archivoSalida = args[i];
+                break;
+        }
+    }
+
+    if (camposOrden.Count == 0)
+        throw new Exception("Debe especificar al menos un campo de ordenamiento");
+
+    return new AppConfig(archivoEntrada, archivoSalida, delimitador, sinEncabezado, camposOrden);
+}
+
 record SortField(string Name, bool Numeric, bool Descending);
 
 record AppConfig(
