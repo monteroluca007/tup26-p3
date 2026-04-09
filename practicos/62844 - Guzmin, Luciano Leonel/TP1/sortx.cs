a277145c5937858aa1c54d3375a45521a3ca01a7
LucianoLeonelGuzman <guzmanlucianoleonel@gmail.com>
2026-04-08 23:14:11 -0300

    creo una funcion MostrarAyuda e implemento comando de ayuda con instrucciones de uso

--- practicos/62844 - Guzmin, Luciano Leonel/TP1/sortx.cs
+++ practicos/62844 - Guzmin, Luciano Leonel/TP1/sortx.cs
@@ -239,6 +239,26 @@ void EscribirSalida(string texto, AppConfig config)
         Console.WriteLine(texto);
 }
 
+void MostrarAyuda()
+{
+    Console.WriteLine(@"
+Indicaciones para usar sortx:
+
+sortx [archivoEntrada [archivoSalida]] -b campo[:tipo[:orden]]
+
+Ejemplo:
+sortx empleados.csv -b apellido
+sortx empleados.csv -b edad:num:desc
+
+Opciones:
+  -b, --by            Campo de ordenamiento
+  -i, --input         Archivo de entrada
+  -o, --output        Archivo de salida
+  -d, --delimiter     Delimitador (ej: , | \t)
+  -nh, --no-header    Indica que no hay encabezado
+  -h, --help          Mostrar ayuda
+");
+}
 
 record SortField(string Name, bool Numeric, bool Descending);
 
