4d9d910d23519eba09129792c758e226333c141b
Bruno Busnelli <bbusnelli1@gmail.com>
2026-04-14 20:24:02 -0300

    parseo basico de argumentos de entrada y salida

--- practicos/63493 - Busnelli, Bruno/TP1/sortx.cs
+++ practicos/63493 - Busnelli, Bruno/TP1/sortx.cs
@@ -18,6 +18,49 @@ catch (Exception ex)
     Console.Error.WriteLine(ex.Message);
 }
 
+//ParseArgs
+AppConfig ParseArgs(string[] args)
+{
+    string? input = null;
+    string? output = null;
+    string delimiter = ",";
+    bool noHeader = false;
+    var sortFields = new List<SortField>();
+
+    int i = 0;
+    if (i < args.Length && !args[i].StartsWith("-"))
+    {
+        input = args[i++];
+    }
+    if (i < args.Length && !args[i].StartsWith("-"))
+    {
+        output = args[i++];
+    }
+
+    while (i < args.Length)
+    {
+        var arg = args[i];
+        switch (arg)
+        {
+            case "-i":
+            case "--input":
+                input = args[++i];
+                break;
+
+            case "-o":
+            case "--output":
+                output = args[++i];
+                break;
+
+            default:
+                throw new Exception($"Argumento desconocido: {arg}");
+        }
+        i++;
+    }
+
+    return new AppConfig(input, output, delimiter, noHeader, sortFields);
+}
+
 record SortField(string Name, bool Numeric, bool Descending);
 
 record AppConfig(
