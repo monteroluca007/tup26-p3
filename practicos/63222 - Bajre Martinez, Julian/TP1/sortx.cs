54fd94f23d94fd35d95e55c96fae9892c99b7c0b
julianbajre-png <julianbajre@gmail.com>
2026-04-15 18:01:49 -0300

    Update sortx.cs
    se lee la entrada. Procesa, ordena, serializa y escribe la salida de todos los datos

--- practicos/63222 - Bajre Martinez, Julian/TP1/sortx.cs
+++ practicos/63222 - Bajre Martinez, Julian/TP1/sortx.cs
@@ -1,7 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
-// sortx [input [output]] [-b|--by campo[:tipo[:orden]]]...
-//       [-i|--input input] [-o|--output output]
-//       [-d|--delimiter delimitador]
-//       [-nh|--no-header] [-h|--help]
-
-Console.WriteLine($"sortx {string.Join(" ", args)}");
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            var config = ParsearArgumentos(args);
+            var texto = LeerEntrada(config);
+            var filas = ParsearDelimitado(texto, config);
+            var filasOrdenadas = OrdenarFilas(filas, config);
+            var salida = Serializar(filasOrdenadas, config);
+            EscribirSalida(config, salida);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+            Environment.Exit(1);
+        }
+    }
+}
